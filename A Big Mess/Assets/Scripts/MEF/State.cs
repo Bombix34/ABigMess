8444c64d8f19196093c2a2e5150a1224bff9acac
Bombixu <bombix34@gmail.com>
2019-11-05 15:10:01 +0100

    [ADD] input manager [ADD] player manager

---
+++ A Big Mess/Assets/Scripts/MEF/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State  
+{
+
+	public string stateName;
+
+	protected ObjectManager curObject;
+
+	public State(ObjectManager curObject)
+    {
+		this.curObject=curObject;
+	}
+	
+	public abstract void Enter();
+	public abstract void Execute();
+	public abstract void Exit();
+    //public abstract void OnMessage(Telegram message);
+}
