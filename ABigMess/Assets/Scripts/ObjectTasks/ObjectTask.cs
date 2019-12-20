f140f76aa097e248ab178f39c907b05e5b58581f
Jorge Nogueira <jorge.noogueira@gmail.com>
2019-12-20 16:42:43 +0100

    [ADD] ObjectTasksManager [ADD] Tasks display on canvas

---
+++ ABigMess/Assets/Scripts/ObjectTasks/ObjectTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ObjectTask 
+{
+    public GameObject gameObject;
+    public Transform desiredPosition;
+    public bool done;
+    public string description;
+}
---
+++ ABigMess/Assets/Scripts/ObjectTasks/ObjectTasksManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ObjectTasksManager : MonoBehaviour
+{
+
+    public List<ObjectTask> objectTasks;
+    public ObjectTask actualTask;
+
+    public TextMeshProUGUI actualTaskText;
+
+    void Start()
+    {
+        GetNextTask();
+    }
+
+    void Update()
+    {
+
+
+        float distance = Vector3.Distance(actualTask.gameObject.transform.position, actualTask.desiredPosition.position);
+        if (distance < 2.0f)
+        {
+            GetNextTask();
+        }
+
+    }
+
+    void GetNextTask()
+    {
+        if (objectTasks.Count > 0)
+        {
+            actualTask = objectTasks[0];
+            actualTaskText.text = actualTask.description;
+            objectTasks.RemoveAt(0);
+        }
+        else
+        {
+            actualTaskText.text = "All tasks done";
+        }
+
+    }
+
+
+
+
+
+}
