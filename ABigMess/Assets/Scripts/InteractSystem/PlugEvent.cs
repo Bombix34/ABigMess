3a11c7b37b83285553f203bdbe4023dd0f02de3c
Jorge Nogueira <jorge.noogueira@gmail.com>
2020-01-09 17:50:55 +0100

    [ADD] Temporary bolts and smoke particles

--- ABigMess/Assets/Scripts/InteractSystem/PlugEvent.cs
+++ ABigMess/Assets/Scripts/InteractSystem/PlugEvent.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "BIGMESS_EVENTS/Plug Event")]
 public class PlugEvent : InteractEvent
 {
+    [SerializeField]
+    ParticleSystem bolts;
+
     public override void InteractionEvent(GameObject objConcerned)
     {
         Debug.Log("Plugging " + objConcerned);
         SetupObjectState(objConcerned);
+        ParticleSystem particles = Instantiate(bolts, objConcerned.transform.position, Quaternion.identity);
+        Destroy(particles.gameObject, 1f);
     }
 }
