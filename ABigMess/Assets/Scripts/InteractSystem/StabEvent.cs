04e9b82b078a373dfdc232819e785d75ee5dfc2d
Bálint <markbalint.a@gmail.com>
2020-01-13 18:25:36 +0100

    Beggining of objects and tools

---
+++ ABigMess/Assets/Scripts/InteractSystem/StabEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "BIGMESS_EVENTS/Stab Event")]
+public class StabEvent : InteractEvent
+{
+    public override void InteractionEvent(GameObject objConcerned)
+    {
+        SetupObjectState(objConcerned);
+    }
+}
