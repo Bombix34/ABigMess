d2906da100fc2e70f1a5ea3c24836a2e726a818c
Bálint <markbalint.a@gmail.com>
2020-01-14 14:31:51 +0100

    interaction system

---
+++ ABigMess/Assets/Scripts/InteractSystem/BurnEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "BIGMESS_EVENTS/Burned Event")]
+public class BurnEvent : InteractEvent
+{
+    public override void InteractionEvent(GameObject objConcerned)
+    {
+        SetupObjectState(objConcerned);
+    }
+}
