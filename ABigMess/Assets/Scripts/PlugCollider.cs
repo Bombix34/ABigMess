572b016e039e0741276acd8ab46fd32377ba3f73
Jorge Nogueira <jorge.noogueira@gmail.com>
2020-01-17 14:40:35 +0100

    [UPDATE] Oven plug unplug

---
+++ ABigMess/Assets/Scripts/PlugCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlugCollider : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("GrabObject")&&other.GetComponent<InteractObject>()!=null)
+        {
+
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("GrabObject") && other.GetComponent<InteractObject>() != null)
+        {
+
+        }
+    }
+}
