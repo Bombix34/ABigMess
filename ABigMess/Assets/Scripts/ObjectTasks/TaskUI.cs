75d07dbdd8474d7e2b1ed2f86a13e1d67223e7c3
Jorge Nogueira <jorge.noogueira@gmail.com>
2020-01-22 16:20:29 +0100

    [ADD] TaskUI

---
+++ ABigMess/Assets/Scripts/ObjectTasks/TaskUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskUI : MonoBehaviour
+{
+    TextMeshProUGUI text;
+
+    void Awake()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void AddImage()
+    {
+
+    }
+
+    public void TaskDone()
+    {
+        GetComponent<Image>().color = Color.yellow;
+    }
+
+    public void TaskUnDone()
+    {
+        GetComponent<Image>().color = Color.white;
+    }
+
+    public void SetText(string description)
+    {
+        text.text = description;
+    }
+}
