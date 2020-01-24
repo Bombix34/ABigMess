db303791cc6a26632861ff62d44e801d9bc38085
Bombixu <bombix34@gmail.com>
2020-01-24 18:39:19 +0100

    [ADD] main menu functionnalities

---
+++ ABigMess/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public string nextScene = "level01";
+
+    private  List<PlayerInputManager> inputManagers;
+
+    private void Start()
+    {
+        inputManagers = new List<PlayerInputManager>();
+        PlayerInputManager[] manager = this.GetComponents<PlayerInputManager>();
+        for(int i = 0; i < manager.Length;++i)
+        {
+            inputManagers.Add(manager[i]);
+        }
+    }
+
+    void Update()
+    {
+        foreach(var inputPlayer in inputManagers)
+        {
+            if(inputPlayer.GetPressAnyButton())
+            {
+                SceneManager.LoadScene(nextScene);
+            }
+        }
+    }
+}
