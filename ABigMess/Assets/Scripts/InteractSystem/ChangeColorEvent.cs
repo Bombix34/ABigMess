409c17e37fb7379dd85d6b7b9bef23a58c82b136
Jorge Nogueira <jorge.noogueira@gmail.com>
2020-01-24 14:59:06 +0100

    [UPDATE] Change color event

--- ABigMess/Assets/Scripts/InteractSystem/ChangeColorEvent.cs
+++ ABigMess/Assets/Scripts/InteractSystem/ChangeColorEvent.cs
@@ -6,9 +6,21 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "BIGMESS_EVENTS/Change Color Event")]
 public class ChangeColorEvent : InteractEvent
 {
+    [SerializeField] List<Color> colors;
+
     public override void InteractionEvent(GameObject objConcerned)
     {
         SetupObjectState(objConcerned);
         TryInstantiateParticleFX(objConcerned);
+
+        if (colors != null && colors.Count > 1)
+        {
+            Color choosenColor = colors[(int)Random.Range(0f, colors.Count)];
+            while(choosenColor == objConcerned.GetComponent<Renderer>().material.color)
+            {
+                choosenColor = colors[(int)Random.Range(0f, colors.Count)];
+            }
+            objConcerned.GetComponent<Renderer>().material.color = choosenColor;
+        }
     }
 }
