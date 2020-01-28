fe25fe22d8b1b994dda6a8c865e4daed0ae8f391
Hugo <bombix34@gmail.com>
2020-01-28 17:36:37 +0100

    [UPDATE] metrics time playing export working

---
+++ ABigMess/Assets/Scripts/Metrics/MetricsInitialiser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetricsInitialiser : MonoBehaviour
+{
+    [SerializeField]
+    private MetricsManager metricsDatas;
+
+    private void Start()
+    {
+        if(metricsDatas.saveMetrics)
+        {
+            metricsDatas.Reset();
+            metricsDatas.InitCSVText();
+        }
+    }
+
+}
