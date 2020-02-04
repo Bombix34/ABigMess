761731c4053186d2a065517eeb4ec4aa3c06b89a
Jorge Nogueira <jorge.noogueira@gmail.com>
2020-02-04 14:39:10 +0100

    [UPDATE] Fixes on appear/disappear tasks

--- ABigMess/Assets/Scripts/ObjectTasks/TaskUI.cs
+++ ABigMess/Assets/Scripts/ObjectTasks/TaskUI.cs
@@ -26,12 +26,18 @@ public class TaskUI : MonoBehaviour
     ObjectTask task;
 
     RectTransform rectTransform;
+    Image image;
+
+    Sequence appearDisappearSequence;
 
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
         rectTransform.anchoredPosition.Set(rectTransform.anchoredPosition.x, rectTransform.sizeDelta.y);
-        GetComponent<Image>().color = Color.white;
+        image.color = Color.white;
+
+        appearDisappearSequence = DOTween.Sequence();
     }
 
     public void DisplayTask(ObjectTask newTask, List<Sprite> list)
@@ -40,7 +46,8 @@ public class TaskUI : MonoBehaviour
         if (list.Count == 0)
         {
             textField.text = newTask.taskName;
-        } else
+        }
+        else
         {
             textField.gameObject.SetActive(false);
 
@@ -56,7 +63,7 @@ public class TaskUI : MonoBehaviour
                 actionIcon.SetActive(list[1] != null ? true : false);
                 actionIcon.GetComponent<Image>().sprite = list[1] != null ? list[1] : actionIcon.GetComponent<Image>().sprite;
             }
-            if (task.eventType==ObjectTask.EventKeyWord.bring)
+            if (task.eventType == ObjectTask.EventKeyWord.bring)
             {
                 destinationIcon.SetActive(true);
                 destinationIcon.GetComponent<Image>().sprite = task.destinationSprite;
@@ -68,29 +75,35 @@ public class TaskUI : MonoBehaviour
         }
     }
 
-    internal void Disapear()
+    public void Disapear()
     {
-        if (task!= null && task.IsDone)
+        if ((task != null && task.IsDone))
         {
-            rectTransform.DOAnchorPosY(rectTransform.sizeDelta.y, 1);
+            appearDisappearSequence.Kill();
+            appearDisappearSequence.Append(image.DOColor(Color.green, 1));
+            appearDisappearSequence.AppendInterval(1f);
+            appearDisappearSequence.Append(rectTransform.DOAnchorPosY(rectTransform.sizeDelta.y, 1));
         }
     }
 
     public void Appear()
     {
-        if (task == null || !task.IsDone)
+        if ((task == null || !task.IsDone))
         {
-            rectTransform.DOAnchorPosY(-rectTransform.sizeDelta.y / 2, 1);
+            appearDisappearSequence.Kill();
+            appearDisappearSequence.Append(image.DOColor(Color.white, 1));
+            appearDisappearSequence.AppendInterval(1f);
+            appearDisappearSequence.Append(rectTransform.DOAnchorPosY(-rectTransform.sizeDelta.y / 2, 1));
         }
     }
 
     public void UpdateNumber()
     {
-        if(task==null )
+        if (task == null)
         {
             return;
         }
-        if(task.showCounterUI)
+        if (task.showCounterUI)
         {
             numberField.text = task.GetCountForInterface().ToString();
         }
@@ -102,7 +115,7 @@ public class TaskUI : MonoBehaviour
         {
             return;
         }
-        GetComponent<Image>().color = task.IsDone ? Color.yellow : Color.white;
+        //image.color = task.IsDone ? Color.green : Color.white;
     }
 
     #region GET/SET
