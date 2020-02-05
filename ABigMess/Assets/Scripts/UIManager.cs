e8ec87c3ba918846387d2d66c99958984bd7012d
Bálint Márk <markbalint.a@gmail.com>
2020-02-05 15:59:07 +0100

    [add] level changes AND WEIRD SHIT

--- ABigMess/Assets/Scripts/UIManager.cs
+++ ABigMess/Assets/Scripts/UIManager.cs
@@ -24,10 +24,18 @@ public class UIManager : Singleton<UIManager>
     private Text transitionText;
     [SerializeField]
     private Image transitionInstruction;
+    [SerializeField]
+    private Image grandmaImage;
+    [SerializeField]
+    private Image bubbleImage;
+
+    private int currentTextPosition = 0;
+
+    [SerializeField]
+    private static float letterApparitionDelay = 0.05f;
 
     private void Start()
     {
-        transitionInstruction.gameObject.SetActive(false);
     }
 
     public void UpdateChronoUI(int minutes, int seconds)
@@ -87,12 +95,17 @@ public class UIManager : Singleton<UIManager>
     public void IntroScreenTransition()
     {
         transitionPanel.SetActive(true);
+        transitionInstruction.gameObject.SetActive(true);
+        grandmaImage.gameObject.SetActive(true);
+        bubbleImage.gameObject.SetActive(true);
+        grandmaImage.color = new Color(grandmaImage.color.r, grandmaImage.color.g, grandmaImage.color.b, 1f);
         TransitionScreen currentLevelTransition = GameManager.Instance.GetCurrentLevel().introScreen;
         //transitionInstruction.gameObject.SetActive(true);
         if (currentLevelTransition != null)
         {
             backgroundTransitionPanel.color = new Color(currentLevelTransition.backgroundColor.r, currentLevelTransition.backgroundColor.g, currentLevelTransition.backgroundColor.b, 1f);
-            transitionText.text = currentLevelTransition.textDescription;
+            transitionInstruction.color = new Color(transitionInstruction.color.r, transitionInstruction.color.g, transitionInstruction.color.b, 1f);
+            transitionText.color = backgroundTransitionPanel.color;
             StartCoroutine(TransitionFade(false));
         }
         else
@@ -121,6 +134,8 @@ public class UIManager : Singleton<UIManager>
             backgroundTransitionPanel.color = new Color(0f, 0f, 0f, 0f);
         }
         transitionInstruction.gameObject.SetActive(false);
+        grandmaImage.gameObject.SetActive(false);
+        bubbleImage.gameObject.SetActive(false);
         transitionText.text = "";
         transitionPanel.SetActive(true);
         StartCoroutine(TransitionFade(true));
@@ -141,16 +156,35 @@ public class UIManager : Singleton<UIManager>
         }
         else
         {
+            TransitionScreen currentLevelTransition = GameManager.Instance.GetCurrentLevel().introScreen;
+            if(currentLevelTransition!=null)
+            {
+                while (currentTextPosition < currentLevelTransition.textDescription.Length)
+                {
+                    transitionText.text += currentLevelTransition.textDescription[currentTextPosition];
+                    currentTextPosition++;
+                    if(GameManager.Instance.PlayersPressValidateInput())
+                    {
+                        transitionText.text = currentLevelTransition.textDescription;
+                        currentTextPosition = currentLevelTransition.textDescription.Length;
+                    }
+                    yield return new WaitForSeconds(letterApparitionDelay);
+                }
+                yield return new WaitForSeconds(0.2f);
+            }
             while (!GameManager.Instance.PlayersPressValidateInput())
             {
                 yield return new WaitForSeconds(0.01f);
             }
+            transitionInstruction.gameObject.SetActive(false);
             GameManager.instance.LaunchLevel();
             float alphaAmount = 1f;
             while (backgroundTransitionPanel.color.a > 0.001f)
             {
                 backgroundTransitionPanel.color = new Color(0f,0f,0f, alphaAmount);
-                transitionInstruction.color = new Color(transitionInstruction.color.r, transitionInstruction.color.g, transitionInstruction.color.b, alphaAmount * 2f);
+                grandmaImage.color = new Color(grandmaImage.color.r, grandmaImage.color.g, grandmaImage.color.b, alphaAmount);
+                bubbleImage.color = new Color(bubbleImage.color.r, bubbleImage.color.g, bubbleImage.color.b, alphaAmount);
+                //ansitionInstruction.color = new Color(transitionInstruction.color.r, transitionInstruction.color.g, transitionInstruction.color.b, alphaAmount * 2f);
                 transitionText.color = new Color(transitionText.color.r, transitionText.color.g, transitionText.color.b, alphaAmount*2f);
                 alphaAmount -= Time.fixedDeltaTime;
                 yield return new WaitForSeconds(0.01f);
