f278ab4673e4cee701cade8866a85be6169f0c41
Jorge Nogueira <jorge.noogueira@gmail.com>
2020-02-10 12:00:58 +0100

    [ADD] Burnt material slot on BurnEvent

--- ABigMess/Assets/Scripts/ObjectStates/Burnt.cs
+++ ABigMess/Assets/Scripts/ObjectStates/Burnt.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class Burnt : MonoBehaviour
 {
     Color defaultColor;
+    Material defaultMaterial;
     ObjectState objectState;
     
     void Start()
     {
         defaultColor = GetComponent<Renderer>().material.color;
+        defaultMaterial = GetComponent<Renderer>().material;
         objectState = GetComponent<ObjectState>();
 
         GetComponent<Renderer>().material.color = Color.black;
     }
 
+    public void BurnMaterial(Material material)
+    {
+        GetComponent<Renderer>().material.color = defaultColor;
+        GetComponent<Renderer>().material = material;
+    }
+
     private void OnDestroy()
     {
         GetComponent<Renderer>().material.color = defaultColor;
+        GetComponent<Renderer>().material = defaultMaterial;
     }
 }
