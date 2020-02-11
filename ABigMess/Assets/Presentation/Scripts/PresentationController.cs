c94c534f2111775b5d97840cbd5c43036b64872a
Hugo <bombix34@gmail.com>
2020-02-11 23:49:50 +0100

    [ADD] first presentation push

---
+++ ABigMess/Assets/Presentation/Scripts/PresentationController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentationController : MonoBehaviour
+{
+    public PresentationLevels levelsDatas;
+
+    public PresentationTransition transitionManager;
+
+    private PresentationInputsManager inputs;
+
+    public bool isFirstScene = false;
+
+    private void Start()
+    {
+        inputs = GetComponent<PresentationInputsManager>();
+        if(isFirstScene)
+        {
+            levelsDatas.curIndexScene = 0;
+        }
+        if(transitionManager!=null)
+        {
+            transitionManager.StartSceneTransition();
+        }
+    }
+
+    private void Update()
+    {
+        UpdateInputs();
+    }
+
+    private void UpdateInputs()
+    {
+        if(inputs.ReloadSceneInput())
+        {
+            levelsDatas.ReloadScene();
+        }
+        if(inputs.NextSceneInput())
+        {
+            StartCoroutine(LoadScene(true));
+        }
+        if(inputs.PreviousSceneInput())
+        {
+            StartCoroutine(LoadScene(false));
+        }
+    }
+
+    public IEnumerator LoadScene(bool isNext)
+    {
+        if(transitionManager!=null)
+        {
+            transitionManager.LeaveSceneTransition();
+            yield return new WaitForSeconds(0.75f);
+        }
+        if(isNext)
+        {
+            levelsDatas.LoadNextScene();
+        }
+        else
+        {
+            levelsDatas.LoadPreviousScene();
+        }
+    }
+}
---
+++ ABigMess/Assets/Presentation/Scripts/PresentationTransition.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using DG.Tweening;
+
+public class PresentationTransition : MonoBehaviour
+{
+    [SerializeField]
+    private Color transitionScreenColorIn;
+    [SerializeField]
+    private Color transitionScreenColorOut;
+    [SerializeField]
+    GameObject transitionPanel;
+    [SerializeField]
+    private Image backgroundTransitionPanel;
+
+    public UnityEvent OnStartSceneEvent;
+    public UnityEvent OnLeaveSceneEvent;
+
+    private void Start()
+    {
+        if(OnStartSceneEvent==null)
+        {
+            OnStartSceneEvent = new UnityEvent();
+        }
+        if(OnLeaveSceneEvent==null)
+        {
+            OnLeaveSceneEvent = new UnityEvent();
+        }
+    }
+
+    public void LeaveSceneTransition()
+    {
+        OnLeaveSceneEvent.Invoke();
+    }
+
+    public void StartSceneTransition()
+    {
+        OnStartSceneEvent.Invoke();
+    }
+
+
+    public void GoOutTransition()
+    {
+        transitionPanel.SetActive(true);
+        backgroundTransitionPanel.color = transitionScreenColorOut;
+        RectTransform transitionPanelRectTransform = transitionPanel.GetComponent<RectTransform>();
+        transitionPanelRectTransform.DOAnchorPosY(transitionPanelRectTransform.rect.height, 0.6f).SetEase(Ease.Linear);
+        //MusicManager.Instance.TransitionLevel(false);
+    }
+
+    public void GoInTransition()
+    {
+        backgroundTransitionPanel.color = transitionScreenColorIn;
+        RectTransform transitionPanelRectTransform = transitionPanel.GetComponent<RectTransform>();
+        transitionPanelRectTransform.anchoredPosition = new Vector2(0f, transitionPanelRectTransform.rect.height);
+        //transitionPanelRectTransform.DOAnchorPosY(transitionPanelRectTransform.rect.height, 0f);
+        transitionPanelRectTransform.DOAnchorPosY(0, 0.6f).SetEase(Ease.Linear);
+        transitionPanel.SetActive(true);
+    }
+
+
+
+}
