17406803ab2c6f736e03affe9e6ec2f031670e03
Jorge Nogueira <jorge.noogueira@gmail.com>
2020-02-12 14:40:20 +0100

    [UPDATE trigger scenes

---
+++ ABigMess/Assets/Presentation/Scripts/ChangeSceneTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneTrigger : MonoBehaviour
+{
+    public Object sceneToChange;
+
+    public PresentationTransition presentationTransition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            presentationTransition.LeaveSceneTransition();
+        }
+    }
+}
