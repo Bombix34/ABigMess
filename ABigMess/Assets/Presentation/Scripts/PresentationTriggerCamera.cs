47bf9d43130fa3cea9efc33d55916bfdb438d7dd
Bombixu <bombix34@gmail.com>
2020-02-12 14:39:17 +0100

    [ADD] camera change trigger

---
+++ ABigMess/Assets/Presentation/Scripts/PresentationTriggerCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PresentationTriggerCamera : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera cameraToLaunch;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            CameraManager.Instance.SetNewCamera(cameraToLaunch);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CameraManager.Instance.SetNewCamera(cameraToLaunch);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
