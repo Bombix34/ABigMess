39d6ed64d58971a2bb881ffed6504ef6e735130d
Labelpastek <simongirau@gmail.com>
2020-02-17 11:51:29 +0100

    [FIX] Trigger Anim Biscuit

---
+++ ABigMess/Assets/Presentation/Scripts/LevelFakeUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFakeUI : MonoBehaviour
+{
+    private Material levelMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelMat = this.gameObject.GetComponent<Renderer>().material;
+        levelMat.SetColor("_EmissionColor", Color.black);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            levelMat.SetColor("_EmissionColor", Color.white);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            levelMat.SetColor("_EmissionColor", Color.black);
+        }
+    }
+}
